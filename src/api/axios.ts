6c51d4b0cfeb098c6f9bc4a7a8e92f56f259f4ef
Asitha Navaratne <asithan476@gmail.com>
2023-10-15 22:15:03 +0530

    Added request abort signal

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const AxiosInstance = axios.create({
   baseURL: "https://v1.formula-1.api-sports.io",
+  timeout: 5000,
 });
 
 export default AxiosInstance;
--- src/pages/Seasons/Seasons.loader.ts
+++ src/pages/Seasons/Seasons.loader.ts
@@ -1,9 +1,14 @@
-import AxiosInstance from "../../api/axios";
+import { GenericAbortSignal } from "axios";
 
+import AxiosInstance from "../../api/axios";
 import CheckResponseForErrors from "../../helpers/CheckResponseForErrors";
 
-export default async function loader() {
-  const response = await AxiosInstance.get("/seasons");
+export default async function loader({
+  signal,
+}: {
+  signal: GenericAbortSignal;
+}) {
+  const response = await AxiosInstance.get("/seasons", { signal: signal });
 
   return CheckResponseForErrors(response).data;
 }
